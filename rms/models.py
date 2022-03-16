11acb586edca51811e69cb7937d8e8369e109c5d
teburile <1667612243@qq.com>
2022-03-16 11:54:29 +0800

    [SR.002.601] Create Iteration Model (#7)

--- rms/models.py
+++ rms/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Iteration(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    project = models.ForeignKey('ums.Project',on_delete=models.CASCADE)
+    sid = models.IntegerField()
+    title = models.TextField()
+    begin = models.FloatField()
+    end = models.FloatField()
+    disabled = models.BooleanField()
+    createAt = models.FloatField()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['project'])
+        ]
