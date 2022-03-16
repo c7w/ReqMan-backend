ffbb24d74a0810e572fa4c1a6ccc83aae7dec123
高焕昂 <2020010951@secoder.net>
2022-03-16 02:53:12 +0000

    [SR.002.003] Finish sub-branch testing

--- backend/settings.py
+++ backend/settings.py
@@ -13,13 +13,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import yaml
 
-file = open('config.yml.dev', 'r', encoding='utf-8')
+try:
+    file = open('config.yml', 'r', encoding='utf-8')
+except FileNotFoundError as e:
+    file = open('config.yml.dev', 'r', encoding='utf-8')
 env = yaml.load(file, Loader=yaml.SafeLoader)
 # print(env)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -36,7 +38,6 @@ if '*' in env['site']['allowed_hosts']:
 else:
     ALLOWED_HOSTS = env['site']['allowed_hosts']
     CORS_ALLOWED_ORIGINS = env['site']['allowed_hosts']
-
 
 # Application definition
 
@@ -85,7 +86,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -110,7 +110,6 @@ else:
         }
     }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -129,7 +128,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -142,7 +140,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
