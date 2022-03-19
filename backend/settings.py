fcfea40af82a64d49fd116c57f9a943ef5c07899
c7w <admin@cc7w.cf>
2022-03-20 00:59:48 +0800

    [SR.002.005] Modify mounting strategies for backend config (#16)

--- backend/settings.py
+++ backend/settings.py
@@ -13,10 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import yaml
 
-try:
-    file = open('config.yml', 'r', encoding='utf-8')
-except FileNotFoundError as e:
-    file = open('config.yml.dev', 'r', encoding='utf-8')
+file = open('config.yml', 'r', encoding='utf-8')
 env = yaml.load(file, Loader=yaml.SafeLoader)
 # print(env)
 
