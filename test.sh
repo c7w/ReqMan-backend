4501fa5929d130f36b997276728b9dc47f2899bb
c7w <admin@cc7w.cf>
2022-03-26 10:41:49 +0800

    [SR.003.906] Bugfix: scheduler & dev database  (#30)

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 pylint --load-plugins pylint_django --fail-under=0 --django-settings-module=backend.settings backend rms ums rdts
-coverage run --source backend,ums,rms,rdts -m pytest --junit-xml=xunit-reports/xunit-result.xml
+coverage run --source backend,ums,rms,rdts,utils -m pytest --junit-xml=xunit-reports/xunit-result.xml
 coverage xml -o coverage-reports/coverage.xml
 coverage report
