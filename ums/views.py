fa26a37002d16f80a639f83821f8098dc83dfcd2
刘明道 <2020011156@secoder.net>
2022-03-30 13:04:38 +0800

    [SR.003.605.I] Add user join project through invitation (#35)

--- ums/views.py
+++ ums/views.py
@@ -9,6 +9,7 @@ from ums.utils import *
 from django.forms.models import model_to_dict
 from utils.throttle import GeneralThrottle, SpecialThrottle
 from utils.permissions import GeneralPermission, project_rights
+from django.conf import settings
 
 DEFAULT_INVITED_ROLE = "member"
 
@@ -56,7 +57,7 @@ class UserViewSet(viewsets.ViewSet):
             if not relation:
                 return Response({"code": 2})
 
-        print(name_valid(name))
+        # print(name_valid(name))
         if (
             name_valid(name)
             and not name_exist(name)
@@ -108,29 +109,15 @@ class UserViewSet(viewsets.ViewSet):
     @action(detail=False, methods=["POST"])
     def logout(self, req: Request):
         if not req.user:
-            return Response({"code": 1})
+            return Response({"code": 1})  # convention in API
 
         disable_session_id(req.auth["sessionId"])
         return Response({"code": 0})
 
     @action(detail=False)
+    @require_login
     def user(self, req: Request):
-        if not req.user:
-            return FAIL
-        return Response(
-            {
-                "code": 0,
-                "data": {
-                    "user": user_to_list(req.user),
-                    "projects": [
-                        model_to_dict(p, exclude=["disabled"])
-                        for p in req.user.project.all()
-                    ],
-                    "schedule": {"done": [], "wip": [], "todo": []},
-                },
-                "avatar": req.user.avatar,
-            }
-        )
+        return Response(user_detail(req.user))
 
     @project_rights(Role.SUPERMASTER)
     @action(detail=False, methods=["POST"])
@@ -158,7 +145,7 @@ class UserViewSet(viewsets.ViewSet):
         relation.delete()
         return SUCC
 
-    @project_rights(Role.SUPERMASTER)
+    @project_rights([Role.SUPERMASTER, Role.SYS])
     @action(detail=False, methods=["POST"])
     def project_add_user(self, req: Request):
         info = proj_user_assoc(req)
@@ -177,15 +164,14 @@ class UserViewSet(viewsets.ViewSet):
         return SUCC
 
     @action(detail=False, methods=["POST"])
+    @require_login
     def create_project(self, req: Request):
-        if not req.user:
-            return FAIL
         title = require(req.data, "title")
         description = require(req.data, "description")
         avatar = req.data.get("avatar")
 
         proj = Project.objects.create(
-            title=title, description=description, avatar=avatar
+            title=title, description=description, avatar=avatar if avatar else ""
         )
         UserProjectAssociation.objects.create(
             project=proj, user=req.user, role="supermaster"
@@ -221,7 +207,7 @@ class UserViewSet(viewsets.ViewSet):
     @action(detail=False, methods=["POST"])
     def upload_project_avatar(self, req: Request):
         proj = req.auth["proj"]
-        print(req.data)
+        # print(req.data)
         avatar = require(req.data, "avatar")
 
         proj.avatar = avatar
@@ -236,6 +222,7 @@ class UserViewSet(viewsets.ViewSet):
 
         if inv:
             inv.invitation = gen_invitation()
+            inv.save()
         else:
             inv = ProjectInvitationAssociation.objects.create(
                 project=proj, role=DEFAULT_INVITED_ROLE, invitation=gen_invitation()
@@ -255,10 +242,8 @@ class UserViewSet(viewsets.ViewSet):
         return Response({"code": 0, "data": {"invitation": inv.invitation}})
 
     @action(detail=False, methods=["POST"])
+    @require_login
     def modify_password(self, req: Request):
-        if not req.user:
-            return FAIL
-
         prev = require(req.data, "prev")
         curr = require(req.data, "curr")
 
@@ -270,10 +255,8 @@ class UserViewSet(viewsets.ViewSet):
         return SUCC
 
     @action(detail=False, methods=["POST"])
+    @require_login
     def upload_user_avatar(self, req: Request):
-        if not req.user:
-            return FAIL
-
         avatar = require(req.data, "avatar")
 
         req.user.avatar = avatar
@@ -283,10 +266,8 @@ class UserViewSet(viewsets.ViewSet):
         return SUCC
 
     @action(detail=False, methods=["POST"])
+    @require_login
     def user_join_project_invitation(self, req: Request):
-        if not req.user:
-            return FAIL
-
         invitation = require(req.data, "invitation")
 
         relation = ProjectInvitationAssociation.objects.filter(
@@ -295,12 +276,16 @@ class UserViewSet(viewsets.ViewSet):
         if not relation:
             return Response({"code": 2})
 
+        if (
+            UserProjectAssociation.objects.filter(
+                user=req.user, project=relation.project
+            ).first()
+            is not None
+        ):
+            return Response({"code": 1})
+
         UserProjectAssociation.objects.create(
-            user=req.user, project=invitation.project, role=invitation.role
+            user=req.user, project=relation.project, role=relation.role
         )
 
         return SUCC
-
-    @action(detail=False, methods=["POST"])
-    def user_exist(self, req: Request):
-        pass
