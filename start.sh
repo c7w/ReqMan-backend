d837a91e6795f870727bcd73d4352074ae02b80e
刘明道 <2020011156@secoder.net>
2022-05-13 13:12:17 +0800

    [SR.007.613.I] Test for uWSGI deploy (#101)

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-cp ./config/config.yml ./config.yml
+# cp ./config/config.yml ./config.yml
 # python3 manage.py makemigrations ums rms rdts
 python3 manage.py migrate
 python3 manage.py createcachetable
 python3 manage.py schedule &
 python3 manage.py webhook &
 python3 manage.py create &
-python3 manage.py runserver 0.0.0.0:80
+
+uwsgi --ini uwsgi.ini
