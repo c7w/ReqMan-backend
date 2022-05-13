96ca5aad3c10ae7fc2df7b26b72c7a7d0e9d07b5
刘明道 <2020011156@secoder.net>
2022-05-13 14:21:50 +0800

    [SR.007.613.I] Test for uWSGI deploy: start.sh (#101)

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# cp ./config/config.yml ./config.yml
+ cp ./config/config.yml ./config.yml
 # python3 manage.py makemigrations ums rms rdts
 python3 manage.py migrate
 python3 manage.py createcachetable
@@ -8,4 +8,4 @@ python3 manage.py webhook &
 python3 manage.py create &
 python3 manage.py runserver 80
 
-#uwsgi --ini uwsgi.ini
+uwsgi --chdir=/opt/tmp/ --module=backend.wsgi:application  --env DJANGO_SETTINGS_MODULE=backend.settings --master  --http=127.0.0.1:8009   --processes=5   --uid=1000 --gid=2000   --harakiri=20   --max-requests=5000    --vacuum   &
